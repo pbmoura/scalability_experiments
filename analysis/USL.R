e9f9deb79e8b165027e7d1c56df8d2ddc136211b
Paulo Moura <pbmoura@gmail.com>
2015-10-21 16:35:32 -0200

    adding model inference scripts

---
+++ analysis/USL.R
@@ -0,0 +1,61 @@
+usl.prepare <- function (p,X) {
+  C <- X/X[1]
+  pC <- p/C
+  
+  x <- p-1
+  y <- pC-1
+  return(data.frame(x,y))
+}
+
+
+
+usl.inference <- function (p, X) {
+  dataset <- usl.prepare(p, X)
+  x <- dataset$x
+  y <- dataset$y
+  reg <- lm(y ~ 0 + x + I(x^2))
+  a <- reg$coefficients[2]
+  b <- reg$coefficients[1]
+  
+  sigma = b-a
+  kappa = a
+  peak = sqrt((1-sigma)/kappa)
+  
+  return(c(sigma, kappa, summary(reg)$r.squared, peak))
+}
+
+
+
+usl.plot <- function(n, estimated, measured, ...) {
+  max = length(throughput)
+  plot(n, measured, ylab = "capacity", ...)
+  lines(estimated)
+}
+
+usl.formula <- function(xs, sigma, kappa) return(xs/(1+sigma*(xs-1)+xs*kappa*(xs-1)))
+
+usl.chart <- function(limit, n, throughput, ...) {
+  if (length(limit) == 1)
+    ii <- c(1:limit)
+  else
+    ii <- limit
+  infer <- usl.inference(n[ii], throughput[ii])
+  estimated <- usl.formula((1:max(n)), infer[1], infer[2])
+  capacity <- throughput/throughput[1]
+  usl.plot(n, estimated, capacity, ...)
+  print(usl.error(estimated, capacity))
+  abline(v=limit)
+  infer
+}
+
+usl.error <- function(estimated, measured) {
+  abs(estimated - measured)
+}
+
+usl.line <- function(limit, throughput, ...) {
+  ii <- c(1:limit)
+  infer <- usl.inference(n[ii], throughput[ii])
+  lines(usl.formula((1:30), infer[1], infer[2]), ...)
+  abline(v=limit, ...)
+  infer
+}
---
+++ analysis/estimate.R
@@ -0,0 +1,20 @@
+#!/usr/bin/Rscript
+source("./USL.R")
+
+args <- commandArgs(TRUE)
+file_name <- args[1] #
+#file_name <- "1n_5_5000_1n_300_50.client"
+sample <- args[2]
+#sample <- 8
+m <- read.table(file_name, head=FALSE, sep=" ")
+
+by.run <-   by(m, m$V1, identity, simplify=FALSE)
+times <- sapply(by.run, function(sample) {max(sample$V9+sample$V10) - min(sample$V10)})
+requests <- sapply(by.run, function(sample) {length(sample$V1)})
+throughput <- requests / times
+x <- sapply(by.run, function(sample) {unique(sample$V6)}) 
+
+pdf(file=paste0(file_name, ".pdf"))
+usl.chart(sample, x, throughput, xlab = "workers")
+lines(x, usl.formula(x, m[1,7]/m[1,5], m[1,8]/m[1,5]), lty=2)
+dev.off()
