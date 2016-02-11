42388f13c536892a43a54badcac000440aabcd0e
Paulo Moura <pbmoura@gmail.com>
2016-02-11 18:05:31 -0200

    individual scripts for pool and queue size comparison

--- analysis/compare-pool.R
+++ analysis/compare-pool.R
@@ -1,19 +1,36 @@
 #!/usr/bin/Rscript
-
 #setwd("~/github/scalability_experiments/analysis")
+source("./common.R")
 
 args <- commandArgs(TRUE)
 file_name1 <- args[1]    
 file_name2 <- args[2]    
-#file_name1 <- "./data/step2/load2_30.mon"
-#file_name2 <- "./data/step2/elastic2_30.mon"
+file_name3 <- args[3]
+
+#file_name1 <- './data/step2/single3b_20.ad'
+#file_name2 <- './data/step2/elastic3b_20.ad'
+#file_name3 <- './data/step2/elastic3b_20.hand'
+
 
 data1 <- read.table(file_name1)
 data2 <- read.table(file_name2)
+data3 <- read.table(file_name3)
 
+wd <- workload(data3)
 time_sec1 <- (data1$V1-data1[1,1])/1000
 time_sec2 <- (data2$V1-data2[1,1])/1000
 
-plot(time_sec1, data1$V4, type="l", col="red", xlab="time (sec.)", ylab="pool size", ylim=c(0,max(data1$V4, data2$V4)))
+
+par(mar = c(5,5,2,5))
+plot(time_sec1, data1$V4, type="l", col="red", xlab="time (sec.)", ylab="pool size", ylim=c(0,max(data1$V4, data2$V4)), xlim=c(0,max(time_sec1, time_sec2)))
 lines(time_sec2, data2$V4, col="blue")
-legend(x = 0, y = max(data1$V4), legend = c("USL", "single-step"), col=c("blue", "red"), lty=1)
+plot_over(wd, "workload", type="l", xlim=c(0,max(time_sec1, time_sec2)), ylim=c(0,max(wd$intervals[wd$intervals != Inf])))
+legend("topright", legend = c("USL", "single-step", "workload"), col=c("blue", "red", "black"), lty=1)
+
+legend(x = 0, y = 5, legend = c("USL", "single-step", "workload"), col=c("blue", "red", "black"), lty=1, bty = 'n')
+
+avg(time_sec1, data1$V4)
+avg(time_sec2, data2$V4)
+
+integral(time_sec1, data1$V4)
+integral(time_sec2, data2$V4)
