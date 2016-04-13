b2406d7c5bf0a878c8837bc725df3a743b45f6c2
Paulo Moura <pbmoura@gmail.com>
2016-04-13 20:52:24 +0200

    replacing fork by pthread

--- src/trace-player.c
+++ src/trace-player.c
@@ -1,12 +1,38 @@
 #include "common.c"
+#include "Linked_list.c"
+
+#define MAX 100000
+
+int counter = 0;
+char* load_balancer;
+
+void *request(void* arg) {
+	unsigned long delay = *((unsigned*)arg);
+	long st, duration;
+	int sock, reply;
+
+	st = time_millis();
+	sock = connectTo(load_balancer, PORT_LB);
+	write(sock, &counter, sizeof(int));
+	read(sock, &reply, sizeof(int));
+	close(sock);
+	duration = time_millis() - st;
+	printf("%ld %lu %ld %d\n", st, delay, duration, reply);
+}
 
 int main(int argc, char* argv[]) {
-	char* load_balancer = argv[1];
+	load_balancer = argv[1];
 	char* file_name = argv[2];
-	int sock, counter = 0, i, reply;
+	int i;
+	int ret;
 	FILE *fd;
 	unsigned long delay;
-	long int st, duration;
+	pthread_t *t1;
+	Linked_list *threads, *node;
+	void *res;
+
+	threads = createList(NULL, NULL);
+	
 	fd = fopen(file_name,"r");
 	if (fd == NULL) {
 		fprintf(stdout, "Error opening file %s\n", file_name);
@@ -15,20 +41,21 @@ int main(int argc, char* argv[]) {
 
 
 	while(EOF != fscanf(fd, "%lu\n", &delay)) {
-		fprintf(stderr, "waiting %ld\n", delay);
+		fprintf(stderr, "%lu waiting %ld\n", time_millis(), delay);
 		counter++;
 		usleep(delay);
-		if (!fork()) {
-			st = time_millis();
-			sock = connectTo(load_balancer, PORT_LB);
-			write(sock, &counter, sizeof(int));
-			read(sock, &reply, sizeof(int));
-			close(sock);
-			duration = time_millis() - st;
-			printf("%ld %lu %ld %d\n", st, delay, duration, reply);
-			return 0;
-		}
+		t1 = malloc(sizeof(pthread_t));
+		ret = pthread_create(t1, NULL, request, (void *) &delay);
+		if (ret != 0)
+			fprintf(stderr, "ERROR creating thread %i\n", ret);
+		else
+			insertAfter(t1, threads);
 	}
-	for(i=0; i<counter; i++)
-		wait(NULL);
+	fprintf(stderr, "%lu workload finished\n", time_millis());
+	for(node = threads->next; node != NULL; node = node->next) {
+		fprintf(stderr, "%lu join ", time_millis());
+		t1 = (pthread_t*)(node->value);
+		fprintf(stderr, "%ld\n", t1);
+		pthread_join(*t1, &res);
+	}
 }
