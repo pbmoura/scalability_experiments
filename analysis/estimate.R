7887af624a1f6a15f0a2da4c01f47642830f372c
Paulo Moura <pbmoura@gmail.com>
2016-04-18 15:34:28 -0300

    small updates

--- analysis/estimate.R
+++ analysis/estimate.R
@@ -1,22 +1,24 @@
 #!/usr/bin/Rscript
-
+#setwd("~/github/scalability_experiments/analysis")
 source("./USL.R")
 
 args <- commandArgs(TRUE)
-file_name <- args[1] #
-sample <- args[2]
+file_name <- args[1] #database
+sample <- args[2] #samples to use model estimation
+#file_name <- "./data/step3/20n_5_0.2_1n_20_800_300b.client"
+#sample <- 15
 m <- read.table(file_name, head=FALSE, sep=" ")
 
 by.run <-   by(m, m$V11, identity, simplify=FALSE)
 times <- sapply(by.run, function(sample) {max(sample$V9+sample$V10) - min(sample$V10)})
 requests <- sapply(by.run, function(sample) {length(sample$V1)})
 throughput <- requests / times
-x <- sapply(by.run, function(sample) {unique(sample$V11)}) 
+x <- sapply(by.run, function(sample) {unique(sample$V5)})
 
 pdf(file=paste0(file_name, ".pdf"))
 estimate <- usl.chart(sample, x, throughput, xlab = "workers")
-lines(x, usl.formula(x, m[1,7], m[1,8]), lty=2)
-usl.peak(m[1,7], m[1,8])
+lines(x, usl.formula(x, m[1,7]/1000000/m[1,4], m[1,8]/1000000/m[1,4]), lty=2)
+usl.peak(m[1,7]/1000000/m[1,4], m[1,8]/1000000/m[1,4])
 usl.peak(estimate[1], estimate[2])
 
 dev.off()
--- analysis/estimate_new.R
+++ analysis/estimate_new.R
@@ -5,16 +5,25 @@ source("./USL.R")
 
 args <- commandArgs(TRUE)
 file_name <- args[1] 
-#file_name <- "./data/1n_5_5_1_4_300_10.client"
+#file_name <- "./data/step3/1n_5_0.2_1_20_800_300.client"
 m <- read.table(file_name, head=FALSE, sep=" ")
 
 by.run <-   by(m, m$V11, identity, simplify=FALSE)
 times <- sapply(by.run, function(sample) {max(sample$V9+sample$V10) - min(sample$V10)})
 requests <- sapply(by.run, function(sample) {length(sample$V1)})
-throughput <- requests / times
+throughput <- requests / (times/1000)
 
-plot(throughput*1000)
+plot(throughput)
 
 n <- which.max(throughput)
+n<-20
 throughput[n]
 median(m$V9[1:(sum(1:n)*m[1,3])])
+
+
+
+
+
+the_run <- data.frame(by.run[20])
+the_run[order(the_run[,10]),]
+
