7485288abec1533b4828e28bbb67a9f488200582
Paulo Moura <pbmoura@gmail.com>
2016-05-31 22:04:36 +0200

    builtin thread pool

--- src/trace-player-pool.c
+++ src/trace-player-pool.c
@@ -1,38 +1,46 @@
 #include <signal.h>
 #include "common.c"
-#include "ThreadPool.c"
+#include "Queue.c"
+
+#define NUM_THREADS 3000
 
 char* load_balancer;
 unsigned long returned = 0;
 sem_t *sem, *sem_result;
+Queue *queue;
 
 typedef struct req_arg {
 	unsigned long counter, delay;
 }req_arg;
 
-void request(void* arg) {
-	unsigned long delay = ((struct req_arg*)arg)->delay;
-	unsigned long counter = ((struct req_arg*)arg)->counter;
+void* request(void* arg) {
+	struct req_arg *args;
 	long st, duration;
-	int sock;
+	int sock, queue_size;
 	unsigned long reply;
 
-	fprintf(stderr, "%lu running request %lu after %lu\n", time_millis(), counter, delay);
+	while(1) {
+		DequeueElement(queue, (void**)&args);
+		queue_size = QueueSize(queue);
 
-	st = time_millis();
-	sock = connectTo(load_balancer, PORT_LB, "to lb");
-	write(sock, &counter, sizeof(counter));
-	read(sock, &reply, sizeof(reply));
-	duration = time_millis() - st;
-	close(sock);
-	sem_wait(sem_result);
-	fprintf(stdout, "%ld %lu %ld %lu\n", st, delay, duration, reply);
-	fflush(stdout);
-	sem_post(sem_result);
-	sem_wait(sem);
-	returned++;
-	sem_post(sem);
-	free(arg);
+		fprintf(stderr, "%lu running request %lu after %lu\n", time_millis(), args->counter, args->delay);
+		fprintf(stderr, "queue size: %d\n", queue_size);
+
+		st = time_millis();
+		sock = connectTo(load_balancer, PORT_LB, "to lb");
+		write(sock, &(args->counter), sizeof(args->counter));
+		read(sock, &reply, sizeof(reply));
+		duration = time_millis() - st;
+		close(sock);
+		sem_wait(sem_result);
+		fprintf(stdout, "%ld %lu %ld %lu\n", st, args->delay, duration, reply);
+		fflush(stdout);
+		sem_post(sem_result);
+		sem_wait(sem);
+		returned++;
+		sem_post(sem);
+		free(args);
+	}
 }
 
 int main(int argc, char* argv[]) {
@@ -43,31 +51,39 @@ int main(int argc, char* argv[]) {
 	unsigned long delay;
 	unsigned long counter = 0;
 	struct req_arg *args;
-	struct ThreadPool *pool;
-	struct Task *aTask;
-
+	pthread_t t1;
+	int cr_t;
+	time_t t;
 	//signal(SIGPIPE, SIG_IGN);
+	
+	fprintf(stderr, "%ld load balancer: '%s'\n", time_millis(), load_balancer);
 
-	pool = CreateThreadPool(2000);
+	queue = createQueue(sizeof(args));
 
+	fprintf(stderr, "%ld created queue\n", time_millis());
+	sem = createsemaphore("/sem_trace", 1);
+	sem_result = createsemaphore("/sem_result", 1);
+
+	fprintf(stderr, "%ld created semaphores\n", time_millis());
 	fd = fopen(file_name,"r");
 	if (fd == NULL) {
 		fprintf(stderr, "Error opening file %s\n", file_name);
 		exit(1);
 	}
 
-	sem = createsemaphore("/sem_trace", 1);
-	sem_result = createsemaphore("/sem_result", 1);
-
+	fprintf(stderr, "%ld trace file opened\n", time_millis());
+	for (i=0; i<NUM_THREADS; i++) 
+		if ((cr_t = pthread_create(&t1, NULL, request, NULL)) != 0)
+			fprintf(stderr, "ERROR creating thread: %i\n", cr_t);
+	fprintf(stderr, "%ld created threads\n", time_millis());
+	//srand((unsigned) time(&t));	
+	//usleep(rand()*10);
 	while(EOF != fscanf(fd, "%lu\n", &delay)) {
 		usleep(delay);
 		args = malloc(sizeof(struct req_arg));
 		args->counter = counter++;
 		args->delay = delay;
-		aTask = malloc(sizeof(Task));
-		aTask->function = *request;
-		aTask->arg = args;
-		AddTask(pool, aTask);
+		Enqueue(queue, (void*)args);
 	}
 	fprintf(stderr, "%lu workload finished\n", time_millis());
 	while (returned < counter) {
@@ -75,5 +91,4 @@ int main(int argc, char* argv[]) {
 		sleep(1);
 	}
 	fprintf(stderr, "finished with %lu of %lu\n", returned, counter);
-	DestroyThreadPool(pool);
 }
