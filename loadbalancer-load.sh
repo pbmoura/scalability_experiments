77dbeb253c8d99611b95c409b4df0694c00f4758
Paulo Moura <pbmoura@gmail.com>
2016-08-23 23:51:13 +0200

    step size as parameter

--- loadbalancer-load.sh
+++ loadbalancer-load.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sar -o /var/scratch/$USER/sar/$5.lb 1 900 > /dev/null &
-bin/load $1 $2 $3 $4 1>>~/scratch/results/$5.lb 2>>~/scratch/logs/$5.lb
+bin/load $1 $2 $3 $4 3 1>>~/scratch/results/$5.lb 2>>~/scratch/logs/$5.lb
 #killall sar
