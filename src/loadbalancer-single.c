279b276b739548c4581ff9f6060d127cff316ef2
Paulo Moura <pbmoura@gmail.com>
2017-03-29 20:03:50 +0200

    step size as parameter

--- src/loadbalancer-single.c
+++ src/loadbalancer-single.c
@@ -1,11 +1,13 @@
 #include "loadbalancer-elastic.c"
 
 double limit_max, limit_min;
+int pool_step;
 
 void init(int argc, char *argv[]) {
 	fprintf(stderr, "init %s, %s\n", argv[3], argv[4]);
 	limit_min = atof(argv[3]);
 	limit_max = atof(argv[4]);
+	pool_step = atoi(argv[5]);
 	fprintf(stderr, "load limits %f - %f	\n", limit_min, limit_max);
 }
 
@@ -14,9 +16,9 @@ void verify_num_workers() {
 	double requests_per_worker = (double)load/num_workers;
 	fprintf(stderr, "%ld requests per worker %f\n", time_millis(), requests_per_worker);
 	if (requests_per_worker >= limit_max)
-		request_workers(pool_manager, 1);
+		request_workers(pool_manager, pool_step);
 	else if (requests_per_worker <= limit_min && num_workers > 1)
-		release_workers(pool_manager, -1);
+		release_workers(pool_manager, -pool_step);
 
 }
 
